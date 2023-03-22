4a86057e550465e14139cc6c8aa6d123320d72ba
Max <pixelcat2211@gmail.com>
2023-03-22 23:07:46 +0500

    edit header

--- users/forms.py
+++ users/forms.py
@@ -3,17 +3,17 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, User
 from .models import CustomUser
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput())
-    email = forms.EmailField(label='e-Mail', widget=forms.EmailInput())
-    city = forms.CharField(label='Город', widget=forms.TextInput())
-    street = forms.CharField(label='Улица', widget=forms.TextInput())
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Повторить пароль', widget=forms.PasswordInput())
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'row'}))
+    email = forms.EmailField(label='e-Mail', widget=forms.EmailInput(attrs={'class': 'row'}))
+    city = forms.CharField(label='Город', widget=forms.TextInput(attrs={'class': 'row'}))
+    street = forms.CharField(label='Улица', widget=forms.TextInput(attrs={'class': 'row'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'row'}))
+    password2 = forms.CharField(label='Повторить пароль', widget=forms.PasswordInput(attrs={'class': 'row'}))
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'city', 'street', 'password1', 'password1')
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput())
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'row'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'row'}))
