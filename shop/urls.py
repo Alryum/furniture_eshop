4e3fbb5eb1e35c90699ed7fbe50b8a3f906613ce
Max <pixelcat2211@gmail.com>
2023-03-26 21:45:36 +0500

    добавлена корзина / изменены шаблоны

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, ProductListView, DetailProductView
+from .views import index, cart_detail, cart_remove, cart_add, ProductListView, DetailProductView
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('cart/', cart_detail, name='cart_detail'),
+    path('cart/add/<int:pk>', cart_add, name='cart_add'),
+    path('cart/remove/<int:pk>', cart_remove, name='cart_remove'),
     path('shop/', ProductListView.as_view(), name='shop'),
-    path('product/<int:pk>', DetailProductView.as_view(), name='detail_product'),
+    path('product/<int:pk>/', DetailProductView.as_view(), name='detail_product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -11,16 +11,28 @@ def index(request):
 
 
 @require_POST
-def cart_add(request, product_id):
+def cart_add(request, pk):
     cart = Cart(request)
-    product = get_object_or_404(Furniture, id=product_id)
+    product = get_object_or_404(Furniture, id=pk)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    return redirect('cart_detail')
+
+
+def cart_remove(request, pk):
+    cart = Cart(request)
+    product = get_object_or_404(Furniture, id=pk)
+    cart.remove(product)
+    return redirect('cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'shop/cart.html', {'cart': cart})
 
 
 class ProductListView(ListView):
@@ -34,3 +46,8 @@ class DetailProductView(DetailView):
     model = Furniture
     template_name = 'shop/detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart_product_form'] = CartAddProductForm()
+        return context
