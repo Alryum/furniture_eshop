918d8c692d384545473d3eafc6bbcc6bf011c5db
Max <pixelcat2211@gmail.com>
2023-04-01 14:21:19 +0500

    добавлены оформления заказов

--- furniture_shop/urls.py
+++ furniture_shop/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('orders.urls')),
     path('', include('shop.urls')),
     path('', include('users.urls')),
 ]
