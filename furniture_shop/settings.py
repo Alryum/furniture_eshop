647d04e8f161b7d734d160ce6c072a6e9996fdf3
Max <pixelcat2211@gmail.com>
2023-05-03 16:31:24 +0500

    drf init

--- furniture_shop/settings.py
+++ furniture_shop/settings.py
@@ -29,10 +29,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # ---
+    # local
     'users',
     'shop',
     'orders',
+    # 3rd party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -83,9 +85,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -135,3 +134,10 @@ CART_SESSION_ID = 'cart'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
