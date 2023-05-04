66310c468d4762ab57ffecf76a9aa437248e8b33
Max <pixelcat2211@gmail.com>
2023-05-04 19:52:59 +0500

    =AllowAny

--- furniture_shop/settings.py
+++ furniture_shop/settings.py
@@ -138,6 +138,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ]
 }
